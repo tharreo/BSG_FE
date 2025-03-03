70a43c296b6d91eed8b73a28b9d5f212c059c190
tharreo <s22110546@student.unklab.ac.id>
2025-03-03 23:19:26 +0800

    ubah

--- src/components/CommercialCreditTabButton.tsx
+++ src/components/CommercialCreditTabButton.tsx
@@ -1,6 +1,7 @@
 import { CardActionArea } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../routes/routes.ts';
+import { t } from 'i18next';
 
 export function CommercialCreditTabButton(props: IProps) {
   return (
@@ -10,7 +11,7 @@ export function CommercialCreditTabButton(props: IProps) {
           <div
             className={` border px-4 flex items-center justify-center py-2 rounded-l-xl ${props.activeIndex === 'SPKK' ? 'bg-red-100/30 text-primary-dark font-semibold border-red-800' : 'bg-white'}`}
           >
-            <h1>PK KMP</h1>
+            <h1>{t('KMP')}</h1>
           </div>
         </CardActionArea>
       </Link>
@@ -19,7 +20,7 @@ export function CommercialCreditTabButton(props: IProps) {
           <div
             className={`bg-white border px-4 flex items-center justify-center py-2 rounded-r-xl ${props.activeIndex === 'PK KUR' ? 'bg-red-100/30 text-primary-dark font-semibold border-red-800' : 'bg-white'}`}
           >
-            <h1>PK KUR</h1>
+            <h1>{t('KUR')}</h1>
           </div>
         </CardActionArea>
       </Link>
